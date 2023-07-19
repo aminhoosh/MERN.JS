fe044b21c3dd06f889d6405ab828a60618d83a10
SoliTheUnholy <soheilzss82@gmail.com>
2023-07-19 17:50:40 +0330

    jalase aval

--- aval.js
+++ aval.js
@@ -1,3 +1,3 @@
-let num1 = +prompt("Enter the first number!")
-let num2 = +prompt("Enter the first number!")
-console.log(num1+num2)
+let number = +prompt("Enter the first number!")
+let number2 = +prompt("Enter the first number!")
+console.log(number+number2)
